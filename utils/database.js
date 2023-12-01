12a2782e5318066e8410c6a866c246172e2487d7
Nakul Aeran <aerannnakul@gmail.com>
2023-12-02 00:37:18 +0530

    first commit

---
+++ utils/database.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const uri = "mongodb+srv://nakul:-978u_V2mZzXTiH@cluster0.ud3ycxn.mongodb.net/?retryWrites=true&w=majority";
+
+
+let isConnected = false; // track the connection
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(`Hello ${error}`);
+  }
+}
